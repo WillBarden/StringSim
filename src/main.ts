ffb2c12499b6ed9ca9d684a87d7cde60929519df
WillBarden <wfbarden@gmail.com>
2019-12-09 22:05:27 -0500

    Add some whitespace to make the demo code more readable

--- src/main.ts
+++ src/main.ts
@@ -18,17 +18,10 @@ interface SearchCriteria {
   effectiveDate?: Date;
 };
 
-const recordEquality = (record1: Record, record2: Record): boolean => {
-  return record1.contractId === record2.contractId &&
-    record1.memberId === record2.memberId &&
-    record1.efDat === record2.efDat;
-};
 
-const searchEquality = (criteria: SearchCriteria, record: Record): boolean => {
-  return record.contractId.includes(criteria.contractId) &&
-    (!criteria.memberId || record.memberId.includes(criteria.memberId)) &&
-    (!criteria.effectiveDate || moment(criteria.effectiveDate).isSameOrBefore(moment(record.efDat)))
-};
+
+
+
 
 
 const initRecord = (
@@ -47,7 +40,23 @@ const printRes = (msg: string, results: object | object[]) => {
 
 const printNewlines = (n: number) => times(n, () => console.log());
 
+
 export const main = async () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Setting up the mock API
   const initialValues: Record[] = [
     initRecord(0, '678', '01', new Date(2004, 11, 2), false, 'UFR'),
     initRecord(2, '678', '02', new Date(2008, 3, 2), false, 'UFR', new Date(2014, 4, 5)),
@@ -56,10 +65,30 @@ export const main = async () => {
     initRecord(4.54, '645', '02', new Date(2006, 2, 15), true, 'YXU'),
     initRecord(8, '645', '03', new Date(2007, 1, 18), false, 'YXU', new Date(2008, 2, 11)),
   ];
+  const recordEquality = (record1: Record, record2: Record): boolean => {
+    return record1.contractId === record2.contractId &&
+      record1.memberId === record2.memberId &&
+      record1.efDat === record2.efDat;
+  };
+
+  const searchEquality = (criteria: SearchCriteria, record: Record): boolean => {
+    return record.contractId.includes(criteria.contractId) &&
+      (!criteria.memberId || record.memberId.includes(criteria.memberId)) &&
+      (!criteria.effectiveDate || moment(criteria.effectiveDate).isSameOrBefore(moment(record.efDat)))
+  };
 
   const api = new MockApi<Record, SearchCriteria>(initialValues, recordEquality, searchEquality);
   printRes('All Records', api.sync_search());
 
+
+
+
+
+
+
+
+
+  // Some mock operations
   let record: Record;
   let criteria: SearchCriteria;
 
@@ -80,26 +109,55 @@ export const main = async () => {
     effectiveDate: record.efDat
   };
 
+
+
+
+
+
   printNewlines(3);
   printRes('Search results for record before it was added', api.sync_search(criteria));
   printRes('Record to add', record);
   await api.add(record);
   printRes('New record', api.sync_search(criteria));
 
+
+
+
+
+
+
+
   printNewlines(3);
   printRes('Record to edit by changing "amountPaid" to 12.00', record);
   await api.edit({ ...record, amountPaid: 12});
   printRes('Edited record', api.sync_search(criteria));
+
+
+
+
 
   printNewlines(3);
   printRes('Record to void', record);
   await api.void(record);
   printRes('Voided record', api.sync_search(criteria));
 
+
+
+
+
+
   printNewlines(3);
   printRes('Record to end-date', record);
   await api.endDate(record);
   printRes('End-dated record', api.sync_search(criteria));
+
+
+
+
+
+
+
+
 
   printNewlines(3);
   printRes('Record to delete', record);
